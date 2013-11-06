8bb5c0ca3e296ca3d0907612a4a6dac0681218a9
akhileshraperthy <akhileshraperthy@gmail.com>
2013-11-05 22:41:53 -0800

    ipl13 Match Details

---
+++ Resources/ui/common/baseui/dd.js
@@ -0,0 +1,108 @@
+/**
+ * @author akhilesh raperthy
+ */
+function dd(title) {
+	var fileName = "data/dd-players.json";
+	var file = Titanium.Filesystem.getFile(Titanium.Filesystem.resourcesDirectory, fileName);
+	var preParseData = (file.read().text);
+	var response = JSON.parse(preParseData);
+	var self = Ti.UI.createWindow({
+		title : 'Most Fours',
+		backgroundImage : '/images/tableview/easycustom/gradientBackground.png'
+
+	});
+	var data = [];
+
+	var section = Ti.UI.createTableViewSection();
+	data.push(section);
+
+	// ROW 1
+	var row1 = Ti.UI.createTableViewRow({
+		selectionStyle:Titanium.UI.iPhone.TableViewCellSelectionStyle.NONE
+	});
+	row1.backgroundImage = '/images/tableview/easycustom/topRow.png';
+	row1.height = 90;
+	var image1 = Ti.UI.createImageView({
+		height : 60,
+		width : 60,
+		left : 10,
+		image : '/images/tableview/dd-l.png'
+
+	});
+	row1.add(image1);
+
+	var item1 = Ti.UI.createLabel({
+
+		text : 'Delhi Daredevils',
+		font : {
+			fontSize : 20
+		},
+		top : 20,
+		left : 80,
+		height : 30,
+		width : 210
+	});
+	row1.add(item1);
+
+	section.add(row1);
+
+	for ( c = 0; c < response.length; c++) {
+
+		var row = Ti.UI.createTableViewRow({
+			selectionStyle:Titanium.UI.iPhone.TableViewCellSelectionStyle.NONE
+
+		});
+		if(c==response.length-1)
+		row.backgroundImage = '/images/tableview/easycustom/bottomRow.png';
+		else
+		row.backgroundImage = '/images/tableview/easycustom/middleRow.png';
+		row1.height = 90;
+
+		var player = Ti.UI.createLabel({
+
+			text : response[c].dd,
+			font : {
+				fontSize : 20
+			},
+			top : 5,
+			left : 10,
+			height : 40,
+			width : 210
+		});
+		row.add(player);
+
+		section.add(row);
+
+	}
+
+	var tableview = Titanium.UI.createTableView({
+		data : data,
+		style : Titanium.UI.iPhone.TableViewStyle.GROUPED,
+		backgroundColor : 'transparent',
+		separatorColor : '#390A0E'
+	});
+	tableview.addEventListener('click', function(e) {
+		if (e.rowData.test) {
+			var ExampleWindow = require(e.rowData.test), win = new ExampleWindow({
+				title : e.rowData.title,
+				containingTab : self.containingTab,
+				tabGroup : self.tabGroup
+			});
+			if (Ti.Platform.name == "android") {
+
+			} else {
+				win.backgroundColor = "#fff";
+				win.barColor = "#111";
+			}
+
+			self.containingTab.open(win, {
+				animated : true
+			});
+		}
+	});
+	self.add(tableview);
+
+	return self;
+}
+
+module.exports = dd;
---
+++ Resources/ui/common/livescore.js
@@ -0,0 +1,145 @@
+/**
+ * @author akhilesh raperthy
+ */
+function LiveScore(title) {
+	var url = 'http://synd.cricbuzz.com/score-gadget/gadget-scores-feed.xml';
+	//'http://static.espncricinfo.com/rss/livescores.xml';//'http://www.cs.odu.edu/~araperth/ipl13/feeds.xml'
+	var self = Ti.UI.createWindow({
+		title : title,
+		backgroundImage : '/images/tableview/easycustom/gradientBackground.png',
+		barColor : 'black'
+	});
+
+	var data;
+	var tableview;
+	var x = 0;
+	
+	function displayItems(items) {
+		var item = 0;
+		var title = 0;
+		for (var c = 0; c < items.length; c++) {
+			item = items.item(c);
+			title = item.getElementsByTagName("header").item(0).text;
+			score = item.getElementsByTagName("description").item(0).text;
+			if (c == 0) {
+				var row = Ti.UI.createTableViewRow({
+					height : 80,
+					selectionStyle:Titanium.UI.iPhone.TableViewCellSelectionStyle.NONE
+				});
+				row.backgroundImage = '/images/tableview/easycustom/topRow.png';
+				var label = Ti.UI.createLabel({
+					text : title,
+					left : 10,
+					top : 5,
+					bottom : 5,
+					right : 5
+				});
+				row.add(label);
+				var description = Ti.UI.createLabel({
+					text : score,
+					left : 10,
+					top : 35,
+					bottom : 5,
+					right : 5
+				});
+				row.add(description);
+				data[x++] = row;
+				row.url = item.getElementsByTagName("url-link").item(c).text;
+			}
+			if (c > 0) {
+
+				var row = Ti.UI.createTableViewRow({
+					selectionStyle:Titanium.UI.iPhone.TableViewCellSelectionStyle.NONE,
+					height : 80
+				});
+				row.backgroundImage = '/images/tableview/easycustom/middleRow.png';
+				var label = Ti.UI.createLabel({
+					text : title,
+					left : 10,
+					top : 5,
+					bottom : 5,
+					right : 5
+				});
+				row.add(label);
+				var description = Ti.UI.createLabel({
+					text : score,
+					left : 10,
+					top : 35,
+					bottom : 5,
+					right : 5
+				});
+				row.add(description);
+				data[x++] = row;
+				row.url = item.getElementsByTagName("url-link").item(0).text;
+			}
+		}
+		tableview = Titanium.UI.createTableView({
+			style : Titanium.UI.iPhone.TableViewStyle.NONE,
+			separatorStyle : Ti.UI.iPhone.TableViewSeparatorStyle.NONE,
+			data:data,
+			backgroundImage:'/images/tableview/easycustom/gradientBackground.png'
+		});
+		//tableview.data = data;
+		self.add(tableview);
+		tableview.addEventListener('click', function(e) {
+			var w = Ti.UI.createWindow({
+				barColor : 'black'
+			});
+			var wb = Ti.UI.createWebView({
+				url : e.row.url
+			});
+			w.add(wb);
+			var b = Titanium.UI.createButton({
+				title : 'Close',
+				style : Titanium.UI.iPhone.SystemButtonStyle.PLAIN
+			});
+			w.setLeftNavButton(b);
+			
+			w.open({
+				modal : true
+			});
+			b.addEventListener('click', function() {
+				w.close();
+				w =null;
+			});
+		});
+	}
+
+	function loadRSSFeed(url) {
+		data = [];
+		var xhr = Ti.Network.createHTTPClient();
+		//"http://static.espncricinfo.com/rss/livescores.xml"
+		xhr.open("GET", url);
+		xhr.onload = function() {
+			try {
+				var doc = this.responseXML.documentElement;
+				var items = doc.getElementsByTagName("match");
+				displayItems(items);
+
+			} catch(E) {
+				Ti.API.info(E);
+			}
+		};
+
+		xhr.send();
+	}
+
+	var r = Titanium.UI.createButton({
+		systemButton : Titanium.UI.iPhone.SystemButton.REFRESH
+	});
+	r.addEventListener('click', function() {
+		// reload feed
+		loadRSSFeed(url);
+	});
+	self.setRightNavButton(r);
+
+	self.addEventListener('open', function() {
+		// load the feed
+		loadRSSFeed(url);
+	});
+
+	return self;
+
+}
+
+module.exports = LiveScore;
